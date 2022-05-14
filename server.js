6e0c3b0fcdce7b69d970ab1f2a61b6fef9915182
Luis <LTCodes520@gmail.com>
2022-05-14 13:28:24 -0700

    updated server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
-const campsiteRouter = require('./routes/campsite/campsiteRouter');
-const promotionRouter = require('./routes/promotion/promotionRouter');
-const partnerRouter = require('./routes/partner/partnerRouter');
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = 3000;
